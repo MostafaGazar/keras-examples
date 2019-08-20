641ed276d340d70e7ffe2dd1f04d2b790f1eff49
Mostafa Gazar <mmegazar@gmail.com>
2019-08-21 09:23:41 +1200

    Update gc scripts

--- tasks/gc/02-start-gc-instance.sh
+++ tasks/gc/02-start-gc-instance.sh
@@ -3,6 +3,3 @@ set -x #echo on
 
 # Start instance
 gcloud compute instances start --zone=$'us-central1-b' 'keras'
-
-# Connect to it
-gcloud compute ssh --zone=$'us-central1-b' jupyter@$'keras' -- -L 8080:localhost:8080
